0d11d56a06bd8fee30281c6bb07e37577a0d8f77
Tim Binney <tjbinney@fullsail.edu>
2014-02-12 11:26:37 -0500

    Changed and layed outline for project

--- Binney_Timothy_Assignment2.js
+++ Binney_Timothy_Assignment2.js
@@ -1,1 +1,69 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+// Timothy Binney
+//SDI Project 2
+// 1402 Term Number
+
+
+// Initial variables
+
+
+
+
+
+
+
+
+
+
+
+// Prompt
+
+
+
+
+
+
+
+
+
+
+// Procedure
+
+
+
+
+
+
+
+// Confirm
+
+
+
+
+
+
+
+
+
+
+
+// Boolean Function
+
+
+
+
+
+
+
+
+
+// Number Function
+
+
+
+
+
+
+
+
+// String Function
